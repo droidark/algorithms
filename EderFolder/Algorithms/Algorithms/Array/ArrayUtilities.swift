f2d93ded28a0f2e004f60185a072d438bf6cb4de
ederMorelos <morelos1904@gmail.com>
2020-12-07 01:37:58 -0800

    Rotate matrix 90 degrees

--- EderFolder/Algorithms/Algorithms/Array/ArrayUtilities.swift
+++ EderFolder/Algorithms/Algorithms/Array/ArrayUtilities.swift
@@ -11,6 +11,40 @@ import Foundation
 class ArrayUtilities {
     typealias Position = (x:Int, y:Int)
     
+    /*
+     * Time Complexity O(n*2)
+     * Space Complexity O(1)
+     * Example: number of elements(3)
+     * temp1: Initial (0,0) -> Position will move to the right, Final (0,3)
+     * temp2: Initial (0,3) -> Position will move to the bottom, Final (3,3)
+     * temp3: Initial (3,3) -> Position will move to the left, Final (3,0)
+     * temp4: Initial (3,0) -> Position will move to the top, Final (0,0)
+     */
+    
+    public static func rotateMatrix(input: inout[[Int]]){
+        let matrixSize = input.count - 1
+        
+        for i in 0..<Int(input.count/2) {
+            for j in i..<matrixSize-i {
+                let temp1 = input[i][j]
+                let temp2 = input[j][matrixSize-i]
+                let temp3 = input[matrixSize-i][matrixSize-j]
+                let temp4 = input[matrixSize-j][i]
+                
+                input[i][j] = temp4
+                input[j][matrixSize-i] = temp1
+                input[matrixSize-i][matrixSize-j] = temp2
+                input[matrixSize-j][i] = temp3
+            }
+        }
+    }
+    
+    /*
+     * Eder's solution
+     * Time Complexity O(n*2)
+     * Space Complexity O(n*2)
+     */
+    
     public static func matrixSpiralOrder(input: [[Int]]) -> [Int] {
         var result = [Int]()
         var level = 0
@@ -25,6 +59,7 @@ class ArrayUtilities {
     
     /*
      * Level es numero de niveles de afuera hacia dentro
+     * Eder's solution
      */
     
     private static func levelArray(input: [[Int]], level: Int) -> [Int]? {
