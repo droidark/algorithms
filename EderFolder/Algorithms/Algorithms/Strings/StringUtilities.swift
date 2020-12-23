3bfae644c627fb16a1258d1870b7c6163cd8b1cb
ederMorelos <morelos1904@gmail.com>
2020-12-23 17:54:48 -0600

    Check if string is Palindrome

--- EderFolder/Algorithms/Algorithms/Strings/StringUtilities.swift
+++ EderFolder/Algorithms/Algorithms/Strings/StringUtilities.swift
@@ -47,4 +47,67 @@ extension String {
         return result * sign
     }
     
+    func isPalindrome() -> Bool {
+        let characters = Array(self)
+        var leftIndex = 0
+        var rightIndex = characters.count - 1
+        
+        
+        while leftIndex < rightIndex {
+            while(!characters[leftIndex].isLetter && leftIndex<rightIndex ){
+                leftIndex += 1
+            }
+            
+            while(!characters[rightIndex].isLetter && leftIndex<rightIndex ){
+                rightIndex -= 1
+            }
+            
+            if characters[leftIndex].lowercased() != characters[rightIndex].lowercased() {
+                return false
+            }
+            
+            leftIndex += 1
+            
+            rightIndex -= 1
+        }
+        
+        return true
+    }
+    
+    static func replaceAndRemove(input: inout[String], size: Int) -> [String] {
+        var numberOfA = 0
+        var numberCharacters = 0
+        
+        for index in 0..<size {
+            if input[index] != "b" {
+                input[numberCharacters] = input[index]
+                numberCharacters += 1
+            }
+            
+            if input[index] == "a" {
+                numberOfA += 1
+            }
+        }
+        
+        var indexBackwards = numberCharacters - 1
+        
+        numberCharacters += numberOfA - 1
+        let finalNumberOfElements = numberCharacters
+        
+        while indexBackwards >= 0 {
+            if input[indexBackwards] == "a" {
+               input[numberCharacters] = "d"
+                numberCharacters -= 1
+                input[numberCharacters] = "d"
+                numberCharacters -= 1
+            } else {
+                input[numberCharacters] = input[indexBackwards]
+                numberCharacters -= 1
+            }
+            
+            indexBackwards -= 1
+        }
+        
+        return Array(input[0...finalNumberOfElements])
+    }
 }
--- EderFolder/Algorithms/AlgorithmsTests/StringTestCases.swift
+++ EderFolder/Algorithms/AlgorithmsTests/StringTestCases.swift
@@ -11,6 +11,11 @@ import XCTest
 
 class StringTestCases: XCTestCase {
     
+    /**
+     * Book Elements of Programming Interviews In Java
+     *  Problem 6.2  Base Conversion
+     */
+    
     func testStringToInt() {
         let string = "1234567"
         let string2 = "505050505058"
@@ -35,5 +40,55 @@ class StringTestCases: XCTestCase {
         XCTAssertTrue(String.intToString(input: integer3) == string3)
         XCTAssertTrue(String.intToString(input: integer4) == "987652840")
         XCTAssertTrue(String.intToString(input: integer5) == "84391850")
+    }
+    
+    /**
+     * Book Elements of Programming Interviews In Java
+     *  Problem 6.4  Replace And Remove. Assuming there is enough space to hold the result
+     *  Remove `b` and replace `a`
+     *  Time Complexity O(n)
+     *  Space Complexity 0
+     */
+    
+    func testReplaceAndRemove() {
+        var example1 = ["a","b","c","d"]
+        var example4 = ["b","c","d"]
+        var example5 = ["a","a","c","d","a","a","a","a"]
+        var example7 = ["a","b","c","d"]
+        var example8 = ["a","a","c","d","a","a","a","a","e","f"]
+        
+        XCTAssertTrue(String.replaceAndRemove(input: &example1, size: 4) == ["d","d","c","d"])
+        XCTAssertTrue(String.replaceAndRemove(input: &example4, size: 3) == ["c","d"])
+        XCTAssertTrue(String.replaceAndRemove(input: &example5, size: 5) == ["d","d","d","d","c","d","d","d"])
+        XCTAssertTrue(String.replaceAndRemove(input: &example7, size: 1) == ["d","d"])
+        XCTAssertTrue(String.replaceAndRemove(input: &example8, size: 6) == ["d","d","d","d","c","d","d","d","d","d"])
+    }
+    
+    /**
+     * Book Elements of Programming Interviews In Java
+     *  Problem 6.5 Palindrome
+     *  Time Complexity O(n)
+     *  Space Complexity 0
+     */
+    
+    func testPalindrome() {
+        let example1 = "A man a plan a canal panama"
+        let example2 = "Eder"
+        let example3 = "the house is yellow"
+        let example4 = "A man, a plan, a canal panama"
+        let example5 = "A man, a plan, a canal panama."
+        let example6 = "eeddxddee"
+        let example7 = "eeddddee"
+        let example8 = "eeddxddeh"
+
+        
+        XCTAssertTrue(example1.isPalindrome() == true)
+        XCTAssertTrue(example2.isPalindrome() == false)
+        XCTAssertTrue(example3.isPalindrome() == false)
+        XCTAssertTrue(example4.isPalindrome() == true)
+        XCTAssertTrue(example5.isPalindrome() == true)
+        XCTAssertTrue(example6.isPalindrome() == true)
+        XCTAssertTrue(example7.isPalindrome() == true)
+        XCTAssertTrue(example8.isPalindrome() == false)
     }
 }
