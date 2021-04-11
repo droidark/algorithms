939ac96a7de6a2aa7c2259afb006592f2d3ec23f
droidark <droidark@outlook.com>
2021-04-11 11:27:49 -0500

    The right side is redundant, thus it was removed

--- JavaFolder/code/src/main/java/xyz/krakenkat/app/coding/MoveZerosToEnd.java
+++ JavaFolder/code/src/main/java/xyz/krakenkat/app/coding/MoveZerosToEnd.java
@@ -3,13 +3,6 @@ package xyz.krakenkat.app.coding;
 public class MoveZerosToEnd {
     public int[] move(int[] arr) {
         for(int i = 0; i < arr.length; i++) {
-            // RIGHT
-            if((i + 1) < arr.length && arr[i] == 0 && arr[i + 1] != 0) {
-                arr[i] = arr[i + 1];
-                arr[i + 1] = 0;
-            }
-            
-            // LEFT
             if((i - 1) > -1 && arr[i -1] == 0 && arr[i] != 0) {
                 arr[i - 1] = arr[i];
                 arr[i] = 0;
