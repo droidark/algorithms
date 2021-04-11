a7d06b38d313368994bd6528fefcc958f51e5139
droidark <droidark@outlook.com>
2021-04-11 02:11:08 -0500

    Remove outer if in MoveZerosToEnd

--- JavaFolder/code/src/main/java/xyz/krakenkat/app/coding/MoveZerosToEnd.java
+++ JavaFolder/code/src/main/java/xyz/krakenkat/app/coding/MoveZerosToEnd.java
@@ -4,21 +4,17 @@ public class MoveZerosToEnd {
     public int[] move(int[] arr) {
         for(int i = 0; i < arr.length; i++) {
             // RIGHT
-            if((i + 1) < arr.length) {
-                if(arr[i] == 0 && arr[i + 1] != 0) {
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = 0;
-                }
+            if((i + 1) < arr.length && arr[i] == 0 && arr[i + 1] != 0) {
+                arr[i] = arr[i + 1];
+                arr[i + 1] = 0;
             }
-
+            
             // LEFT
-            if((i - 1) > -1) {
-                if(arr[i -1] == 0 && arr[i] != 0) {
-                    arr[i - 1] = arr[i];
-                    arr[i] = 0;
-                    i-=2;
-                }
-            }
+            if((i - 1) > -1 && arr[i -1] == 0 && arr[i] != 0) {
+                arr[i - 1] = arr[i];
+                arr[i] = 0;
+                i-=2;
+            }            
         }
         return arr;
     }
