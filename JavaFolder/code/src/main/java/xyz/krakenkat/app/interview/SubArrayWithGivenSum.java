8693833b19f7cf3fe3a9aebca1c06bfff53e810e
droidark <droidark@outlook.com>
2023-04-09 00:57:45 -0600

    Fix issues with SubArrayWithGivenSum

--- JavaFolder/code/src/main/java/xyz/krakenkat/app/interview/SubArrayWithGivenSum.java
+++ JavaFolder/code/src/main/java/xyz/krakenkat/app/interview/SubArrayWithGivenSum.java
@@ -1,44 +1,64 @@
 package xyz.krakenkat.app.interview;
 
+/*
+	FIND SUB ARRAY WITH GIVEN SUM
+
+	Given an array arr[] of non-negative integers and an integer sum,
+	find a subarray that adds to a given sum.
+	
+	Example 1:
+  		Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+		Output: Sum found between indexes 2 and 4
+		Explanation: Sum of elements between indices 2 and 4 is 20 + 3 + 10 = 33
+
+	Example 2:
+		Input: arr[] = {1, 4, 0, 0, 3, 10, 5}, sum = 7
+		Output: Sum found between indexes 1 and 4
+		Explanation: Sum of elements between indices 1 and 4 is 4 + 0 + 0 + 3 = 7
+
+	Example 3:
+		Input: arr[] = {1, 4}, sum = 0
+		Output: No subarray found
+		Explanation: There is no subarray with 0 sum
+
+	- - - - - - - - - - -
+	
+	More references, please visit:
+		https://www.geeksforgeeks.org/find-subarray-with-given-sum/
+		https://www.youtube.com/watch?v=Ofl4KgFhLsM
+*/
+
+
 public class SubArrayWithGivenSum {
 
-	public static void main(String[] args) {
+	public int[] getSubArrayWithGivenSum(int[] array, int num) {
+		StringBuilder str = new StringBuilder();
 
-		test(new int[] {
-				10,3,5,8,6,12,20,15,31
-		}, 31);
-		
-	}
-
-	
-	public static void test(int[] array, int num) {
-		StringBuilder str= new StringBuilder();
-		for(int i=0; i < array.length; i++) {
-			int sumVal=0;
-			int count=i;
-			while(count<array.length) {
-				if(sumVal == num) { //primera condicion cuando la suma sea igual al numero a bucar
+		for(int i = 0; i < array.length; i++) {
+			int sumVal = 0, count = i;
+			while(count < array.length) {
+				if (sumVal == num) { // First condition, when the sum is equals to the  number to look.
 					break;
-				}
-				else if(sumVal>num) { // cuando sobrepase el sumVal al n�mero a buscar 
-					str= new StringBuilder(); // se borra el stringbuilder 
-					count--; //se retorno el indice para tomar en cuenta el n�mero que sobrepaso
+				} else if (sumVal > num) { // When sumVal is greater than the number to look.
+					str = new StringBuilder(); // Clean the StringBuilder
+					count--; // The count decrease to consider the exceed number
 					break;
 				}
 				else {
-					sumVal+=array[count];
-					str.append(count+" ");
+					sumVal += array[count];
+					str.append(count + " ");
 				}
 				count++;
 			}
-			if(sumVal==num) {
-				break;   //si encuentra la suma entonces rompe el ciclo for 
+			if(sumVal == num) {
+				break;	//	if num is found, the loop is broke.
 			}
 		}
+
+		String[] strArray = str.toString().split(" ");
 		
-		String[] strArray= str.toString().split(" ");
-		
-		System.out.println("["+strArray[0]+","+strArray[strArray.length-1]+"]");	
+		return strArray.length > 1 
+			? new int[] {Integer.parseInt(strArray[0]), Integer.parseInt(strArray[strArray.length - 1])}
+			: new int[] {0};
 	}
-	
 }
---
+++ JavaFolder/code/src/test/java/xyz/krakenkat/app/interview/SubArrayWithGivenSumTest.java
@@ -0,0 +1,53 @@
+package xyz.krakenkat.app.interview;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SubArrayWithGivenSumTest {
+
+    private final SubArrayWithGivenSum subArrayWithGivenSum = new SubArrayWithGivenSum();
+
+    @Test
+    @DisplayName("Should return [2, 5]")
+    void testOne() {
+        // Given
+        int[] arr = {10, 3, 5, 8, 6, 12, 20, 15, 31};
+		int num = 31;
+
+        // When
+        int[] actual = subArrayWithGivenSum.getSubArrayWithGivenSum(arr, num);
+
+        // Then
+        assertArrayEquals(new int[] {2, 5}, actual);
+    }
+
+    @Test
+    @DisplayName("Should return [1, 4]")
+    void testTwo() {
+        // Given
+        int[] arr = {1, 4, 0, 0, 3, 10, 5};
+		int num = 7;
+
+        // When
+        int[] actual = subArrayWithGivenSum.getSubArrayWithGivenSum(arr, num);
+
+        // Then
+        assertArrayEquals(new int[] {1, 4}, actual);
+    }
+
+    @Test
+    @DisplayName("Should return [0]")
+    void testThree() {
+        // Given
+        int[] arr = {1, 4};
+		int num = 0;
+
+        // When
+        int[] actual = subArrayWithGivenSum.getSubArrayWithGivenSum(arr, num);
+
+        // Then
+        assertArrayEquals(new int[] {0}, actual);
+    }
+}
